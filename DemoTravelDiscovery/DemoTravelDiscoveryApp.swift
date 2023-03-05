4c31eff64677ca8173241b08d9a7286daeb743f8
jjaykim <dnglglmm@gmail.com>
2023-03-05 03:44:44 -0500

    Initial Commit

---
+++ DemoTravelDiscovery/DemoTravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTravelDiscoveryApp.swift
+//  DemoTravelDiscovery
+//
+//  Created by JAY KIM on 2023-03-05.
+//
+
+import SwiftUI
+
+@main
+struct DemoTravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
